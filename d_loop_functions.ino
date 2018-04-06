56d595972309afe813065fff0b6a6e55e7e521a5
frontsidekante <s62963@beuth-hochschule.de>
2018-04-06 12:04:14 +0200

    upda

--- d_loop_functions.ino
+++
@@ -1,9 +0,0 @@
-//author: frontsidekante
-//functions used in loop()
-
-void getUsableGyroData(){
-  mpu.getMotion6(&RawAccX, &RawAccY, &RawAccZ, &RawGyroX, &RawGyroY, &RawGyroZ); 
-  usableGyroX = RawGyroX / 131.0;
-  usableGyroY = RawGyroY / 131.0;
-  //usableGyroZ = RawGyroZ / 131.0;
-}
--- z_test_log.ino
+++ z_test_log.ino
@@ -1,44 +1,15 @@
 //author: frontsidekante
 //functions used for logging values
 
-void printRawMotion6(){
-
-  Serial.print("| RawAccX: ");
-  Serial.print(RawAccX);
-  Serial.print("| RawAccY: ");
-  Serial.print(RawAccY);
-  Serial.print("| RawAccZ: ");
-  Serial.print(RawAccZ);
-  Serial.print("| RawGyroX: ");
-  Serial.print(RawGyroX);
-  Serial.print("| RawGyroY: ");
-  Serial.print(RawGyroY);
-  Serial.print("| RawGyroZ:  ");
-  Serial.println(RawGyroZ);
-  delay(200); 
-}
-
 void printUsableGyroData(){
   
   Serial.print(" | GyroX: ");
-  Serial.print(usableGyroX);
+  Serial.print(gyroX);
   Serial.print(" | GyroY: ");
-  Serial.print(usableGyroY);
-  Serial.print(" | GyroZ: ");
-  Serial.println(usableGyroZ);
+  Serial.print(gyroY);
   delay(200); 
 }
 
-void printUsableAccData(){
-
-  Serial.print(" | AccX: ");
-  Serial.print(usableAccX);
-  Serial.print(" | AccY: ");
-  Serial.print(usableAccY);
-  Serial.print(" | AccZ: ");
-  Serial.println(usableAccZ);
-  delay(200);  
-}
 
 
   
